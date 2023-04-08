5885a26703d2a2129836acb699676241c96de5f9
SHubhamSingh1112 <singhshubhamsingh2208@outlook.com>
2023-04-08 23:55:41 +0530

    Banner Content

--- src/components/Introsection.jsx
+++ src/components/Introsection.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import Blobs from './additions/Blobs'; 
+import Blobs from './additions/Blobs';
 
 export default class Introsection extends Component {
   render() {
     return (
       <>
         <div className='banner'>
-          <Blobs/>
+          <Blobs />
           <div className='intro-content'>
-            <h1>Hello I am Shubham Singh</h1>
-            <p>I am a frontend Developer</p>
+            <h1>Frontend Developer</h1>
+            <p>I'm Shubham Singh, a Frontend Developer with expertise in UX research and a keen eye for UI design. With over 100 live projects under my belt, I'm a creative problem-solver who delivers exceptional user experiences. My collaborative approach and deadline speciality ensure innovative and efficient results that exceed expectations.</p>
           </div>
         </div>
       </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,9 @@ import Box from '@mui/material/Box';
 import { MdFacebook } from "react-icons/md";
 import { AiFillInstagram } from "react-icons/ai";
 import { AiFillLinkedin } from "react-icons/ai";
+import { AiFillCodepenCircle } from "react-icons/ai";
+import { AiFillBehanceCircle } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 
 export default class Navbar extends Component {
   render() {
@@ -14,6 +17,7 @@ export default class Navbar extends Component {
           top: '0',
           left: '0',
           position: 'sticky',
+          zIndex: '1000',
           height: 'auto',
           backgroundColor: 'rgba(200, 200, 200, 0.4)',
           backdropFilter: 'blur(10px)',
@@ -35,9 +39,12 @@ export default class Navbar extends Component {
           </Box>
         </Box>
         <Box sx={{ p: 1, display: 'flex', flex: '1', borderRight: '1px solid whitesmoke' }}>
-            <Link className='social-icons' to="https://www.facebook.com/profile.php?id=100003071391024"><MdFacebook/></Link>
-            <Link className='social-icons' to="https://www.instagram.com/shubham_singh_0292/"><AiFillInstagram/></Link>
-            <Link className='social-icons' to="https://www.linkedin.com/in/11shubhamsingh22/"><AiFillLinkedin/></Link>
+            <Link className='social-icons' target='_blank' to="https://www.linkedin.com/in/11shubhamsingh22/"><AiFillLinkedin/></Link>
+            <Link className='social-icons' target='_blank' to="https://github.com/ShubhamSingh1112/"><AiFillGithub/></Link>
+            <Link className='social-icons' target='_blank' to="https://www.behance.net/shubhamsingh90"><AiFillBehanceCircle/></Link>
+            <Link className='social-icons' target='_blank' to="https://codepen.io/shubhamsingh718356"><AiFillCodepenCircle/></Link>
+            <Link className='social-icons' target='_blank' to="https://www.instagram.com/shubham_singh_0292/"><AiFillInstagram/></Link>
+            <Link className='social-icons' target='_blank' to="https://www.facebook.com/profile.php?id=100003071391024"><MdFacebook/></Link>
         </Box>
         <Box sx={{ p: 1, display: 'flex', flex: '1' }}>
           <Link className="light" to="/Contact">Contact</Link>
