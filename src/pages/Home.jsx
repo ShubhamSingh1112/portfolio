f196c7882b927ef9a0ef1fb948decd8186e0f45a
SHubhamSingh1112 <singhshubhamsingh2208@outlook.com>
2023-04-09 00:25:38 +0530

    About Us Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import Introsection from '../components/Introsection'
 import Portfoliogrid from '../components/Portfoliogrid'
-import Aboutsection from '../components/Aboutsection'
 import Contactform from '../components/Contactform'
 
 export default class Home extends Component {
